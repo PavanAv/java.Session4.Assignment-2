2fbb575cac2ea39b27757d6d08d52e3eadc7c233
PavanAv <pavanaluri2011@gmail.com>
2016-10-25 16:11:59 +0530

    Session4Assignment2
    Session4Assignment2

---
+++ S4Assignment2/src/SortAndInsertArray.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SortAndInsertArray {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int cnt, j, num;		
+		System.out.println("Please enter the length of the array: ");				
+		Scanner sc = new Scanner(System.in);				
+		cnt = sc.nextInt();				
+		int myArr[] = new int[cnt];			
+		int newArr[] = new int[cnt+1];			
+		System.out.println("Please enter the values for the array: ");			
+		for (int i=0; i<cnt; i++){				
+		j = i+1;				
+		System.out.println("Value " + j + ": ");				
+		myArr[i] = sc.nextInt();				
+		}				
+		System.out.println();				
+		System.out.println("The array is: ");				
+		for (int i = 0; i<cnt; i++) {					
+		System.out.println(myArr[i]);				
+		}				
+		System.out.println();				
+		System.out.println("Please enter the number to be inserted: ");				
+		num = sc.nextInt();							
+		newArr[cnt] = num;				
+		System.arraycopy(myArr, 0, newArr, 0, cnt);								
+		System.out.println();				
+		System.out.println("Array after the addition of new element is:");								
+		for (int i=0; i<=cnt; i++){					
+		j = i+1;					
+		System.out.println("Value " + j + ": " + newArr[i]);				
+		}				
+		Arrays.sort(newArr);				
+		System.out.println();				
+		System.out.println("Sorted Array:");				
+		for (int i=0; i<=cnt; i++){					
+		j = i+1;					
+		System.out.println("Value " + j + ": " + newArr[i]);				
+		}
+	}
+}
